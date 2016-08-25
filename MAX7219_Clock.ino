4093cba1ddef3c363eacdebd447a71b1fb7963e8
Butch <butchtabios@live.com>
2016-08-25 15:22:17 +0800

    used setSyncProvider(getNtpTime) to periodically sync to PAGASA

--- MAX7219_Clock.ino
+++ MAX7219_Clock.ino
@@ -10,7 +10,6 @@ char pass[] = "";       // your network password
 
 unsigned int localPort = 2390;      // local port to listen for UDP packets
 
-IPAddress timeServerIP;
 const char* ntpServerName = "ntp.pagasa.dost.gov.ph";
 
 // NTP time stamp is in the first 48 bytes of the message
@@ -182,64 +181,7 @@ void setup() {
 	Serial.print("Local port: ");
 	Serial.println(udp.localPort());
 
-	//get a random server from the pool
-	WiFi.hostByName(ntpServerName, timeServerIP);
-
-	sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-								 // wait to see if a reply is available
-	delay(1000);
-
-	int cb = udp.parsePacket();
-	if (!cb) 
-	{
-		Serial.println("no packet yet");
-	}
-	else 
-	{
-		Serial.print("packet received, length=");
-		Serial.println(cb);
-		// We've received a packet, read the data from it
-		udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-		//the timestamp starts at byte 40 of the received packet and is four bytes,
-		// or two words, long. First, esxtract the two words:
-		unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-		unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-		// combine the four bytes (two words) into a long integer
-		// this is NTP time (seconds since Jan 1 1900):
-		unsigned long secsSince1900 = highWord << 16 | lowWord;
-		Serial.print("Seconds since Jan 1 1900 = ");
-		Serial.println(secsSince1900);
-
-		// now convert NTP time into everyday time:
-		Serial.print("Unix time = ");
-		// Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-		const unsigned long seventyYears = 2208988800UL;
-		// subtract seventy years:
-		unsigned long epoch = secsSince1900 - seventyYears;
-		// print Unix time:
-		Serial.println(epoch);
-
-		long int time_zone = 8*3600l;
-		time_t t = epoch + time_zone;
-		setTime(t);
-
-		// print the hour, minute and second:
-		Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-		Serial.print((epoch % 86400L) / 3600); // print the hour (86400 equals secs per day)
-		Serial.print(':');
-		if (((epoch % 3600) / 60) < 10) {
-			// In the first 10 minutes of each hour, we'll want a leading '0'
-			Serial.print('0');
-		}
-		Serial.print((epoch % 3600) / 60); // print the minute (3600 equals secs per minute)
-		Serial.print(':');
-		if ((epoch % 60) < 10) 
-		{
-			// In the first 10 seconds of each minute, we'll want a leading '0'
-			Serial.print('0');
-		}
-		Serial.println(epoch % 60); // print the second
-	}
+	setSyncProvider(getNtpTime);
 }
 
 void UpdateTime(void);
@@ -280,25 +222,48 @@ void UpdateTime(void)
 	scrollText[6] = '0' + sec10;
 	scrollText[7] = '0' + sec - (sec10 * 10);
 
-	//Serial.print(hour);
-	//Serial.print(':');
-	//Serial.print(min);
-	//Serial.print(':');
-	//Serial.print(sec);
-
 	if (isAM(tm))
 	{
 		scrollText[8] = 'a';
 		scrollText[9] = 'm';
-		//Serial.println("am");
 	}
 	else
 	{
 		scrollText[8] = 'p';
 		scrollText[9] = 'm';
-		//Serial.println("pm");
 	}
 }
+
+const int timeZone = 8;     // PHT
+
+time_t getNtpTime()
+{
+	IPAddress timeServerIP;
+
+	while (udp.parsePacket() > 0); // discard any previously received packets
+	Serial.println("Transmit NTP Request");
+	WiFi.hostByName(ntpServerName, timeServerIP);
+	sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+								 // wait to see if a reply is available
+	uint32_t beginWait = millis();
+	while (millis() - beginWait < 1500) {
+		int size = udp.parsePacket();
+		if (size >= NTP_PACKET_SIZE) {
+			Serial.println("Receive NTP Response");
+			udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+			unsigned long secsSince1900;
+			// convert four bytes starting at location 40 to a long integer
+			secsSince1900 = (unsigned long)packetBuffer[40] << 24;
+			secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+			secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+			secsSince1900 |= (unsigned long)packetBuffer[43];
+			return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+		}
+	}
+	Serial.println("No NTP Response :-(");
+	return 0; // return 0 if unable to get the time
+}
+
 
 // send an NTP request to the time server at the given address
 unsigned long sendNTPpacket(IPAddress& address)
